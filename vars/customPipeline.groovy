032ade35897c12f8a37513e7b4237abf08d1f3d7
administrator <administrator@silentier.com>
2023-05-08 18:55:12 -0400

    Initial commit

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -1,11 +1,6 @@
 def call ( Map popertyInfo ){
-    agent {
-        docker {
-            image 'maven:3.9.0-eclipse-temurin-11'
-            args '-v $HOME/.m2:/root/.m2'
-        }
-    }
-    stage ("In Docker") {
+
+    stage ("Check File") {
         def conf = "app/conf.txt"
         props = readProperties file : conf
 
