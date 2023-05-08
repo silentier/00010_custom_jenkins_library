1431b1e06d93870e70f0ec74ed030068cbe29554
administrator <administrator@silentier.com>
2023-05-08 19:50:33 -0400

    Initial commit

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -1,15 +1,9 @@
 def call ( Map popertyInfo ){
 
-    agent {
-        docker {
-            image 'node:14-alpine'
-            label 'docker-node'
-        }
-    }
 
-      stage("Stages") {
+
           unitaryTest();
           compile();
-      }
+
 
 }
