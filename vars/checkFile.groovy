91a9ffb112251956a7507a5362708c5588c3e76c
administrator <administrator@silentier.com>
2023-05-08 19:12:22 -0400

    Initial commit

--- vars/checkFile.groovy
+++ vars/checkFile.groovy
@@ -3,6 +3,8 @@ def call ( Map popertyInfo ){
     node ("k8s_master") {
 
         stage ("Check File") {
+            checkout scm
+
             def conf = "app/conf.txt"
             props = readProperties file : conf
 
