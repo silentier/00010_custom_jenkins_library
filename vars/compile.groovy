4390c321796b9dd0392f2c0c2a78a43c53752f40
administrator <administrator@silentier.com>
2023-05-08 19:23:31 -0400

    Initial commit

--- vars/compile.groovy
+++ vars/compile.groovy
@@ -3,6 +3,7 @@ def call ( Map popertyInfo ){
     node ("k8s_master") {
 
         stage ("Compile") {
+            checkout scm
 
             def conf = "app/conf.txt"
             props = readProperties file : conf
