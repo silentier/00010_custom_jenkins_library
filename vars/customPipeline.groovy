d38d96727a0ced6c79c573cdcc0dbfa1e8b7c94a
administrator <administrator@silentier.com>
2023-05-08 19:13:22 -0400

    Initial commit

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -2,10 +2,7 @@ def call ( Map popertyInfo ){
 
     node ("k8s_master") {
 
-        stage ("custom Stages") {
-            def conf = "app/conf.txt"
-            props = readProperties file : conf
-
+        stage ("Stages") {
             checkFile();
         }
     }
