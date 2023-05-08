8279dfdd1442f8ef229f0b339c5c2912457c551a
administrator <administrator@silentier.com>
2023-05-08 19:00:05 -0400

    Initial commit

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -1,12 +1,21 @@
 def call ( Map popertyInfo ){
 
-    node ("master") {
+    node {
+        checkout scm
+
+        docker.withRegistry('https://registry.example.com') {
+
+            docker.image('my-custom-image').inside {
+                sh 'make test'
+            }
+        }
+    }
         stage ("custom Stages") {
             def conf = "app/conf.txt"
             props = readProperties file : conf
 
             println props
         }
-    }
+
 
 }
