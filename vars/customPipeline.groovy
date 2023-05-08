0c9920c5f4ea6cbc2293ee631dc4db723e3c5f6b
administrator <administrator@silentier.com>
2023-05-08 19:07:55 -0400

    Initial commit

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -5,7 +5,7 @@ def call ( Map popertyInfo ){
             def conf = "app/conf.txt"
             props = readProperties file : conf
 
-            println props
+            checkFile();
         }
     }
 
