7d2a88a5b71036c5bf47332b22581b39bfd43d27
fcovi <administrator@silentier.com>
2023-05-18 00:14:20 -0400

    Initial commit

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -2,7 +2,7 @@ def call ( Map popertyInfo ){
     node("k8s_master") {
         stage("Stages") {
             compile();
-            unitaryTests()
+            unitaryTest()
             generateDocker();
         }
     }
--- vars/unitaryTest.groovy
+++ vars/unitaryTest.groovy
@@ -8,21 +8,12 @@ def call ( Map popertyInfo ){
             def conf = "app/conf.txt"
             props = readProperties file : conf
 
-            println props
-            println "compileMethod:"+props.testMethod
 
-            switch (props.compileMethod) {
-                case "mvn":
-                    configFileProvider([configFile(fileId: 'd9f13ed0-a67a-4c59-81d9-f6034324ed8b', variable: 'config')]) {
-                        sh ("mvn -version")
-                        sh ("mvn "+props.testMethod+" -s ${config} ")
-                    }
-
-                    break
-                default:
-                    println "default"
-                    break
+            configFileProvider([configFile(fileId: 'd9f13ed0-a67a-4c59-81d9-f6034324ed8b', variable: 'config')]) {
+                sh ("mvn -version")
+                sh ("mvn "+props.testMethod+" -s ${config} ")
             }
+
         }
     }
 }
