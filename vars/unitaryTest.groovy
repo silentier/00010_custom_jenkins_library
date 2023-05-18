e51e2d7f0fb86db8b1f6f3ea6ff0abf1ae4e88c5
fcovi <administrator@silentier.com>
2023-05-18 00:11:33 -0400

    Initial commit

--- vars/unitaryTest.groovy
+++ vars/unitaryTest.groovy
@@ -2,7 +2,7 @@ def call ( Map popertyInfo ){
 
     node ("k8s_master") {
 
-        stage ("Compile") {
+        stage ("Unitary Tests") {
             checkout scm
 
             def conf = "app/conf.txt"
