04a3ba0fcd02f1578862dcc6734fb7ba42685f7e
fcovi <administrator@silentier.com>
2023-05-18 00:51:43 -0400

    Initial commit

--- vars/generateDocker.groovy
+++ vars/generateDocker.groovy
@@ -1,11 +1,16 @@
 def call ( Map popertyInfo ) {
 
+    environment {
+        DOCKERHUB_CREDENTIALS=credentials("dockerhub")
+    }
+
     node("k8s_master") {
-        stage ("Generate Docker") {
+        stage ("Generate Docker and Push") {
             def conf = "app/conf.txt"
             props = readProperties file: conf
 
             sh("ls -la")
+            sh 'docker login -u $DOCKERHUB_CREDENTIALS_USR -p $DOCKERHUB_CREDENTIALS_PSW '
             sh("docker build -t " + props.dockerRepository + ":" + props.deockerDefaultTag + " .")
 
         }
