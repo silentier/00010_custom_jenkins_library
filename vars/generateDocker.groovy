d88bcb2c98ff125500e4495a49af2b0c79119111
fcovi <administrator@silentier.com>
2023-05-20 19:48:34 -0400

    kaniko docker-secret

--- vars/generateDocker.groovy
+++ vars/generateDocker.groovy
@@ -1,30 +1,12 @@
 def call ( Map popertyInfo ){
     podTemplate(yaml: '''
-              kind: Pod
-              metadata:
-                namespace: devops-tools
-              spec:
-                containers:
-                - name: kaniko
-                  image: gcr.io/kaniko-project/executor:v1.6.0-debug
-                  imagePullPolicy: Always
-                  command:
-                  - sleep
-                  args:
-                  - 99d
-                  volumeMounts:
-                    - name: jenkins-docker-cfg
-                      mountPath: /kaniko/.docker
-                volumes:
-                - name: jenkins-docker-cfg
-                  projected:
-                    sources:
-                    - secret:
-                        name: docker-secret
-                        items:
-                          - key: .dockerconfigjson
-                            path: config.json
-
+kind: Pod
+metadata:
+    namespace: devops-tools
+spec:
+  containers:
+  - name: executor
+    image: gcr.io/kaniko-project/executor:latest
 ''')
             {
         node(POD_LABEL) {
