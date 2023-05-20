d4394120aeeeceb2073943bcd84df7c7ddcda503
fcovi <administrator@silentier.com>
2023-05-20 16:50:35 -0400

    compile

--- vars/compile.groovy
+++ vars/compile.groovy
@@ -22,29 +22,36 @@ spec:
 ''') {
         node(POD_LABEL) {
             container('maven') {
+
                 stage("Compile") {
-                    checkout scm
-
-                    sh("cat /etc/os-release")
-
-                    def conf = "app/conf.txt"
-                    props = readProperties file: conf
-
-                    println props
-                    println "compileMethod:" + props.compileMethod
-
-                    switch (props.compileMethod) {
-                        case "mvn":
-                            configFileProvider([configFile(fileId: 'd9f13ed0-a67a-4c59-81d9-f6034324ed8b', variable: 'config')]) {
-                                sh("mvn -version")
-                                sh("mvn " + props.compileCommand + " -s ${config} ")
-                            }
-
-                            break
-                        default:
-                            println "default"
-                            break
+                    when {
+                        expression { (readProperties file: "app/conf.txt").compileEnabled == "true" }
                     }
+                    steps {
+                        checkout scm
+
+                        sh("cat /etc/os-release")
+
+                        def conf = "app/conf.txt"
+                        props = readProperties file: conf
+
+                        println props
+                        println "compileMethod:" + props.compileMethod
+
+                        switch (props.compileMethod) {
+                            case "mvn":
+                                configFileProvider([configFile(fileId: 'd9f13ed0-a67a-4c59-81d9-f6034324ed8b', variable: 'config')]) {
+                                    sh("mvn -version")
+                                    sh("mvn " + props.compileCommand + " -s ${config} ")
+                                }
+
+                                break
+                            default:
+                                println "default"
+                                break
+                        }
+                    }
+
                 }
             }
         }
