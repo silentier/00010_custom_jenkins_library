76bb468823ac2ea442d816d61d65bbf671f9ec7f
fcovi <administrator@silentier.com>
2023-05-20 18:37:30 -0400

    generate docker

--- vars/generateDocker.groovy
+++ vars/generateDocker.groovy
@@ -25,8 +25,16 @@ spec:
                         def conf = "app/conf.txt"
                         props = readProperties file: conf
 
+                        withCredentials([[$class: 'UsernamePasswordMultiBinding',
+                                          credentialsId: 'dockerhub',
+                                          usernameVariable: 'USERNAME',
+                                          passwordVariable: 'PASSWORD']]) {
+                            sh """
+                                docker login -u $USERNAME -p $PASSWORD
+                            """
+                        }
 
-                        sh 'docker login -u $DOCKERHUB_CREDENTIALS_USR -p $DOCKERHUB_CREDENTIALS_PSW '
+
 
                         sh("docker build -t " + props.dockerRepository + ":" + props.deockerDefaultTag + " .")
                         sh("docker push " + props.dockerRepository + ":" + props.deockerDefaultTag + " ")
