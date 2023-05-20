319ab2a7f0199c1cc5e73e871557535999270227
fcovi <administrator@silentier.com>
2023-05-19 20:17:26 -0400

    Initial commit

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -2,6 +2,8 @@ def call ( Map popertyInfo ){
     node("jdk17mvn") {
         stage("Stages") {
 
+            sh("cat /etc/os-release")
+
             compile();
             unitaryTest();
             packageStage();
