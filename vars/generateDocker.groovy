9e849c402d82b0bf7fab1d929943f61d4bf8c9c2
fcovi <administrator@silentier.com>
2023-05-20 17:12:02 -0400

    compile

--- vars/generateDocker.groovy
+++ vars/generateDocker.groovy
@@ -1,21 +1,43 @@
-def call ( Map popertyInfo ) {
+def call ( Map popertyInfo ){
+    podTemplate(yaml: '''
+apiVersion: v1
+kind: Pod
+metadata:
+  namespace: devops-tools
+spec:
+  containers:
+  - name: maven
+    image: silentier/00010_golden_image_slave_jenkins:latest
+    command:
+    - sleep
+    args:
+    - 99d
+    volumeMounts:
+     - mountPath: "/root/.m2/"
+       name: mvn-repository
+  volumes:
+    - name: mvn-repository
+      persistentVolumeClaim:
+        claimName: mvn-repository-vol-claim
+''') {
+        node(POD_LABEL) {
+            container('maven') {
+                stage("Generate docker") {
+                    script {
+                        checkout scm
 
-    node("jdk17mvn") {
-        stage ("Generate Docker and Push") {
-            def conf = "app/conf.txt"
-            props = readProperties file: conf
+                        def conf = "app/conf.txt"
+                        props = readProperties file: conf
 
-            sh("ls -la")
 
-            withCredentials([[$class: 'UsernamePasswordMultiBinding',
-                              credentialsId: 'dockerhub',
-                              usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                sh "docker login -u $USERNAME -p $PASSWORD "
+                        sh 'docker login -u $DOCKERHUB_CREDENTIALS_USR -p $DOCKERHUB_CREDENTIALS_PSW '
+
+                        sh("docker build -t " + props.dockerRepository + ":" + props.deockerDefaultTag + " .")
+                        sh("docker push " + props.dockerRepository + ":" + props.deockerDefaultTag + " ")
+                    }
+                }
             }
-
-
-            sh("docker build -t " + props.dockerRepository + ":" + props.deockerDefaultTag + " .")
-            sh ("docker push " + props.dockerRepository + ":" + props.deockerDefaultTag + " ")
         }
     }
 }
+
