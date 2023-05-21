4e255966f6b89a7a061cd985739607612ede5560
fcovi <administrator@silentier.com>
2023-05-20 21:12:51 -0400

    normal

--- vars/generateDocker.groovy
+++ vars/generateDocker.groovy
@@ -12,41 +12,34 @@ spec:
     - sleep
     args:
     - 99d
-''')
-            {
+''') {
         node(POD_LABEL) {
             container('maven') {
-                stage("Generate and push docker") {
-                    script {
-                        checkout scm
+                stage("generate docker") {
+                    checkout scm
 
-                        sh("cat /etc/os-release")
+                    sh("cat /etc/os-release")
 
+                    def conf = "app/conf.txt"
+                    props = readProperties file: conf
 
-                        def conf = "app/conf.txt"
-                        props = readProperties file: conf
+                    println props
+                    println "testMethod:" + props.testMethod
 
-                        withCredentials([[$class: 'UsernamePasswordMultiBinding',
-                                          credentialsId: 'dockerhub',
-                                          usernameVariable: 'USERNAME',
-                                          passwordVariable: 'PASSWORD']]) {
-                            /*
-                            sh """
-                                docker login -u $USERNAME -p $PASSWORD
-                            """
-                             */
-                            sh("docker build -t " + props.dockerRepository + ":" + props.deockerDefaultTag + " .")
-                        }
+                    switch (props.testMethod) {
+                        case "mvn":
+                            configFileProvider([configFile(fileId: 'd9f13ed0-a67a-4c59-81d9-f6034324ed8b', variable: 'config')]) {
+                                sh("mvn -version")
+                                sh("mvn " + props.testCommand + " -s ${config} ")
+                            }
 
-
-
-                    //sh("docker build -t " + props.dockerRepository + ":" + props.deockerDefaultTag + " .")
-                    // sh("docker push " + props.dockerRepository + ":" + props.deockerDefaultTag + " ")
-
+                            break
+                        default:
+                            println "default"
+                            break
                     }
                 }
             }
         }
     }
 }
-
