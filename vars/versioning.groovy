4ffce7a7b99adc8d44bb615de7de6e7de9b79091
fcovi <administrator@silentier.com>
2023-05-21 21:05:19 -0400

    mvn tag version

--- vars/versioning.groovy
+++ vars/versioning.groovy
@@ -34,6 +34,7 @@ spec:
                         case "mvn":
                             configFileProvider([configFile(fileId: 'd9f13ed0-a67a-4c59-81d9-f6034324ed8b', variable: 'config')]) {
                                 OLD_VERSION=sh(script:"mvn help:evaluate -Dexpression=project.version -q -DforceStdout" , returnStdout: true).trim()
+                                println "OLD_VERSION:"+OLD_VERSION
 
                                 parte1='${parsedVersion.majorVersion}'
                                 parte2='${parsedVersion.minorVersion}'
@@ -41,7 +42,7 @@ spec:
                                 sh("""mvn build-helper:parse-version versions:set -DnewVersion=\\${parte1}.\\${parte2}.\\${parte3} versions:commit -s ${config} """)
 
                                 NEW_VERSION=sh(script:"mvn help:evaluate -Dexpression=project.version -q -DforceStdout" , returnStdout: true).trim()
-
+                                println "NEW_VERSION:"+NEW_VERSION
 
                                 sh("git add .")
                                 sh("git commit -m 'build ${NEW_VERSION}' ")
